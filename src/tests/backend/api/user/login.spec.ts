e283e418b2ab8c7218ab66e031b5bfa9770ce24a
GustavoGomesDias <treeelevengg@gmail.com>
2022-02-11 14:20:44 -0300

    test: should return 404 if user not exists

---
+++ src/tests/backend/api/user/login.spec.ts
@@ -0,0 +1,60 @@
+import { EmailValidatorAdapter } from '../../../../serverless/adapters/services/EmailValidatorAdapter';
+import UserController from '../../../../serverless/api/controllers/User';
+import { badRequest, HttpResponse, notFound } from '../../../../serverless/api/helpers/http';
+import UserRepositoryMocked from '../../../mocks/mockUserRepository';
+
+jest.mock('../../../mocks/mockUserRepository');
+
+const makeEmailValidator = (): EmailValidatorAdapter => {
+  class EmailValidatorStub implements EmailValidatorAdapter {
+    isEmail(email: string): boolean {
+      return true;
+    }
+  }
+
+  return new EmailValidatorStub();
+}
+
+const makeSut = (): UserController => {
+  const emailValidatorStub = makeEmailValidator();
+  return new UserController(emailValidatorStub, UserRepositoryMocked);
+}
+
+
+describe('Handle User Login Tests', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const infos = {
+      email: '',
+      password: 'password',
+    };
+    const userController = makeSut();
+
+    const httpResponse: HttpResponse = await userController.handleLogin(infos);
+
+    expect(httpResponse).toEqual(badRequest('E-mail requerido (a).'));
+  });
+
+  test('Should return 400 if no password is provided', async () => {
+    const infos = {
+      email: 'email@email.com',
+      password: '',
+    };
+    const userController = makeSut();
+
+    const httpResponse: HttpResponse = await userController.handleLogin(infos);
+
+    expect(httpResponse).toEqual(badRequest('Senha requerido (a).'));
+  });
+
+  test('Should return 404 if user not exists', async () => {
+    const infos = {
+      email: 'email@email.com',
+      password: 'password',
+    };
+    jest.spyOn(UserRepositoryMocked, 'findByEmail').mockReturnValueOnce(Promise.resolve(undefined));
+    const userController = makeSut();
+    const httpResponse: HttpResponse = await userController.handleLogin(infos);
+
+    expect(httpResponse).toEqual(notFound('Usuário não existente, considere criar uma conta.'));
+  });
+});
