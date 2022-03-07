5ebc434c64fa8d8f96d5a91866da65b5bd0d5d0a
GustavoGomesDias <treeelevengg@gmail.com>
2022-03-07 16:14:45 -0300

    refactor: fix all lint errors in utils folder

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,8 +1,5 @@
-export const validationField = (field: string) => field === "" || field == " " || field === null || field === undefined;
+export const validationField = (field: string) => field === '' || field === ' ' || field === null || field === undefined;
 
-export const validateEmail = (email: string) =>
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email.toLowerCase());
+export const validateEmail = (email: string) => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email.toLowerCase());
 
-export const validationDay = (day: number) => {
-  return day > 0 && day <= 31;
-}
+export const validationDay = (day: number) => day > 0 && day <= 31;
