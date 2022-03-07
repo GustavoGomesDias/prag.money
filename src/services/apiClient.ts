49cbb6ed6ce77037653d9213f2db6019dbe13479
GustavoGomesDias <treeelevengg@gmail.com>
2022-03-07 16:46:46 -0300

    feat: add custom change according to NODE_ENV

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,12 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { parseCookies } from 'nookies';
+
+const createAPI = (): AxiosInstance => {
+  if (process.env.NODE_ENV === 'production') {
+    return axios.create({
+      baseURL: 'https://pragmoney.vercel.app/api',
+    });
+  }
+
+  return axios.create({
+    baseURL: 'http://localhost:3000/api',
+  });
+};
 
 const getAPICLient = (ctx?: any) => {
   const { authToken } = parseCookies(ctx);
-  const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
-  });
+
+  const api = createAPI();
 
   if (authToken) {
     api.defaults.headers.common.Authorization = `Bearer ${authToken}`;
