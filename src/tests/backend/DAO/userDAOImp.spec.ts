3122ebff4a659f286498184fe7d3db28fe4263f2
GustavoGomesDias <treeelevengg@gmail.com>
2022-03-11 17:08:27 -0300

    refact: change makeSut

--- src/tests/backend/DAO/userDAOImp.spec.ts
+++ src/tests/backend/DAO/userDAOImp.spec.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 import { PrismaClient } from '@prisma/client';
 import UserModel from '../../../serverless/data/models/UserModel';
 import UserDAOImp from '../../../serverless/DAOImp/users/UserDAOImp';
 import mockUserDAOImp from '../../mocks/mockUserDAOImp';
+import EncryptAdapter from '../../../serverless/adapters/services/EncryptAdapter';
 
 jest.mock('../../mocks/mockUserDAOImp');
 
@@ -11,18 +14,41 @@ afterAll(async () => {
   await prisma.$disconnect();
 });
 
+const makeEncrypter = (): EncryptAdapter => {
+  class EncryptStub implements EncryptAdapter {
+    encrypt(password: string): Promise<string> {
+      throw new Error('Method not implemented.');
+    }
+
+    async compare(password: string, passHashed: string): Promise<boolean> {
+      const result = await Promise.resolve(true);
+      return result;
+    }
+  }
+
+  return new EncryptStub();
+};
+
 const makeSut = (): UserDAOImp => mockUserDAOImp;
 
 describe('User Repository test', () => {
+  test('Should call constructor with prisma.user', () => {
+    const encrypter = makeEncrypter();
+    const instance = new UserDAOImp(encrypter);
+
+    // eslint-disable-next-line dot-notation
+    expect(instance['encrypter']).toEqual(encrypter);
+  });
+
   test('Should call addUer with correct values', async () => {
     const req: UserModel = {
       email: 'email@email.com',
       name: 'name',
       password: 'password',
     };
-    const dao = makeSut();
-    const spy = jest.spyOn(dao, 'addUser');
-    await dao.addUser(req);
+
+    const spy = jest.spyOn(mockUserDAOImp, 'addUser');
+    await mockUserDAOImp.addUser(req);
 
     expect(spy).toHaveBeenCalledWith(req);
   });
@@ -33,8 +59,8 @@ describe('User Repository test', () => {
       name: 'name',
       password: 'password',
     };
-    const dao = makeSut();
-    const result = await dao.addUser(req);
+
+    const result = await mockUserDAOImp.addUser(req);
 
     expect(result).toEqual({
       email: 'email@email.com',
@@ -44,17 +70,15 @@ describe('User Repository test', () => {
 
   test('Should call findByEmail with correct email', async () => {
     const req = 'email@email.com';
-    const dao = makeSut();
-    const spy = jest.spyOn(dao, 'findByEmail');
-    await dao.findByEmail(req);
+    const spy = jest.spyOn(mockUserDAOImp, 'findByEmail');
+    await mockUserDAOImp.findByEmail(req);
 
     expect(spy).toHaveBeenCalledWith(req);
   });
 
   test('Should returns account user infos', async () => {
     const req = 'email@email.com';
-    const dao = makeSut();
-    const result = await dao.findByEmail(req);
+    const result = await mockUserDAOImp.findByEmail(req);
 
     expect(result).toEqual({
       id: 1,
