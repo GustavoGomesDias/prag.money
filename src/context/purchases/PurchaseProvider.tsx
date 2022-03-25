b6bcdc97d5f259a7b07d8fe7580b805aad67475b
GustavoGomesDias <treeelevengg@gmail.com>
2022-03-25 17:42:50 -0300

    feat: add loader in PurchaseProvider

--- src/context/purchases/PurchaseProvider.tsx
+++ src/context/purchases/PurchaseProvider.tsx
@@ -1,4 +1,5 @@
-import React, { useMemo, useReducer } from 'react';
+import React, { useMemo, useReducer, useState } from 'react';
+import ModalLoader from '../../components/UI/Loader/ModalLoader';
 import GetForeignInfos from '../../serverless/data/usecases/GetForeignInfos';
 import api from '../../services/fetchAPI/init';
 import PurchaseContext from './PurchaseContext';
@@ -9,17 +10,20 @@ export interface PurchaseProviderProps {
 }
 
 export default function PurchaseProvider({ children }: PurchaseProviderProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [purchasesState, dispatchPurchasesActions] = useReducer(purchaseReducer, {
     purchases: [],
   });
 
   const handleGetPurchasesByPaymentId = async (paymentId: number) => {
+    setIsLoading(true);
     const response = await api.get(`/payment/${paymentId}`);
     const content = response.data.content as Omit<GetForeignInfos, 'payments'>;
     dispatchPurchasesActions({
       type: 'POPULATE_PURCHASELIST',
       purchases: content.purchases,
     });
+    setTimeout(() => setIsLoading(false), 500);
   };
 
   const handleClearPurchaseList = () => {
@@ -37,6 +41,7 @@ export default function PurchaseProvider({ children }: PurchaseProviderProps) {
 
   return (
     <PurchaseContext.Provider value={context}>
+      {isLoading && <ModalLoader isOpen={isLoading} />}
       {children}
     </PurchaseContext.Provider>
   );
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -29,10 +29,6 @@ const Dashboard = ({ payments, error }: DashboardProps): JSX.Element => {
   const purchaseCtx = useContext(PurchaseContext);
 
   useEffect(() => {
-    console.log(purchaseCtx.purchases);
-  }, [purchaseCtx.purchases]);
-
-  useEffect(() => {
     const handlePayment = () => {
       if (error) {
         if (error.statusCode === 400) {
