1dd2d1519fb53b044dec9b87f07fa6b8e1ad7a0e
GustavoGomesDias <treeelevengg@gmail.com>
2022-05-17 14:17:21 -0300

    test: add get payment by id

---
+++ src/tests/backend/api/payment/get.spec.ts
@@ -0,0 +1,54 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable dot-notation */
+import PaymentController from '../../../../serverless/api/controllers/PaymentController';
+import { badRequest, ok, okWithContent } from '../../../../serverless/api/helpers/http';
+import PaymentDAOImp from '../../../../serverless/DAOImp/payment/PaymentDAOImp';
+import PaymentModel from '../../../../serverless/data/models/PaymentModel';
+import { BadRequestError } from '../../../../serverless/error/HttpError';
+
+const makeSut = (): PaymentController => {
+  const daoIMP = new PaymentDAOImp();
+  const controllerStub = new PaymentController(daoIMP);
+  return controllerStub;
+};
+
+describe('Handle Get By Payment Id', () => {
+  test('Should return 400 if invalid user id is provided', async () => {
+    const controllerStub = makeSut();
+    // eslint-disable-next-line prefer-destructuring
+    const entity = new PaymentDAOImp()['entity'];
+    jest.spyOn(entity, 'findUnique').mockImplementationOnce(jest.fn());
+
+    const result = await controllerStub.handleGetPaymentById(-1);
+
+    expect(result).toEqual(badRequest(new BadRequestError('ID inválido.')));
+  });
+
+  test('Should return 200 with content if payment is updated', async () => {
+    const payment: PaymentModel = {
+      nickname: 'nickname',
+      default_value: 800,
+      reset_day: 1,
+      user_id: 1,
+    };
+    // eslint-disable-next-line prefer-destructuring
+    jest.spyOn(PaymentDAOImp.prototype, 'findUnique').mockImplementationOnce(async (info) => {
+      const result = await Promise.resolve({
+        nickname: 'nickname',
+        default_value: 800,
+        reset_day: 1,
+        user_id: 1,
+      });
+
+      return result;
+    });
+
+    const controllerStub = makeSut();
+    const result = await controllerStub.handleGetPaymentById(1);
+
+    expect(result.content).toEqual({ payment });
+
+    expect(result.statusCode).toEqual(200);
+  });
+});
