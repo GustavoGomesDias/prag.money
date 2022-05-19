3dbfb6a09eaa74012b943bea402b4b4775733e5c
GustavoGomesDias <treeelevengg@gmail.com>
2022-05-19 13:53:36 -0300

    test/fix: change format date test

--- src/tests/general/utils/formatDate.spec.ts
+++ src/tests/general/utils/formatDate.spec.ts
@@ -4,8 +4,9 @@ describe('Format Date code util tests', () => {
   test('Should return date in format DD/MM/YYYY', () => {
     const date = new Date('2022-03-28T00:00:00Z');
 
+    const day = date.getDate();
     const formatedDate = formatDate(date);
 
-    expect(formatedDate).toEqual('28/3/2022');
+    expect(formatedDate).toEqual(`${day + 1}/3/2022`);
   });
 });
