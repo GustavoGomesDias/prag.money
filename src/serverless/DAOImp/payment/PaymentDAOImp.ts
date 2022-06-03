541bc3828c128b06ffcbae1c40bac3577791858d
GustavoGomesDias <treeelevengg@gmail.com>
2022-06-03 16:38:33 -0300

    feat: add new financial account control

--- src/serverless/DAOImp/payment/PaymentDAOImp.ts
+++ src/serverless/DAOImp/payment/PaymentDAOImp.ts
@@ -1,8 +1,10 @@
 /* eslint-disable camelcase */
 import { Prisma } from '@prisma/client';
+import { GetDate } from '../../adapters/services/FinancialHelperAdapter';
 import { checkIfExists404code } from '../../api/helpers/Validations';
 import PaymentModel from '../../data/models/PaymentModel';
 import prisma from '../../data/prisma/config';
+import AddAdditionalValue from '../../data/usecases/AddAdditionalValue';
 import GetAcquisitions, { ReturnsAcquisitions } from '../../data/usecases/GetAcquisitions';
 import ExtendGenericDAOImp from '../../infra/DAO/ExtendGenericDAOImp';
 import PaymentDAO from './PaymentDAO';
@@ -41,7 +43,7 @@ Prisma.PaymentFindManyArgs
         nickname: true,
         reset_day: true,
         user_id: true,
-        additional_value: true,
+        current_value: true,
       },
     }) as GetAcquisitions;
 
@@ -72,7 +74,7 @@ Prisma.PaymentFindManyArgs
         nickname: true,
         reset_day: true,
         user_id: true,
-        additional_value: true,
+        current_value: true,
       },
     }) as GetAcquisitions[];
 
@@ -95,5 +97,69 @@ Prisma.PaymentFindManyArgs
       },
     }) as unknown as PaymentModel | undefined | null;
     checkIfExists404code(payment, 'Forma de pagamento não cadastrada.');
+  }
+
+  async updateAccountValueWithBalance(payment: GetAcquisitions) {
+    await this.update({
+      where: {
+        id: payment.id,
+      },
+
+      data: {
+        current_month: new Date(Date.now()).getMonth() + 1,
+        current_value: {
+          increment: payment.default_value,
+        },
+      },
+    });
+  }
+
+  getDayAndMonth(date: Date): GetDate {
+    const createdAt = new Date(date);
+    const day = createdAt.getDate();
+    const month = createdAt.getMonth() + 1;
+
+    return {
+      day, month,
+    };
+  }
+
+  async hasMonthBalance(acquisition: GetAcquisitions): Promise<void> {
+    const currentDate = new Date(Date.now());
+    const { day, month } = this.getDayAndMonth(currentDate);
+    const { reset_day, current_month } = acquisition;
+
+    if (day >= reset_day && month > current_month) {
+      await this.updateAccountValueWithBalance(acquisition);
+    }
+  }
+
+  async resolveHasMonthBalance(payments: GetAcquisitions[]) {
+    const results = [];
+    for (const payment of payments) {
+      results.push(this.hasMonthBalance(payment));
+    }
+    await Promise.all(results);
+  }
+
+  async setMonthBalance(payments: GetAcquisitions | GetAcquisitions[]) {
+    if (Array.isArray(payments)) {
+      await this.resolveHasMonthBalance(payments);
+    } else {
+      await this.hasMonthBalance(payments);
+    }
+  }
+
+  async addAdditionValue(infos: Omit<AddAdditionalValue, 'userId'>) {
+    await this.update({
+      where: {
+        id: infos.paymentId,
+      },
+      data: {
+        current_value: {
+          increment: infos.additionalValue,
+        },
+      },
+    });
   }
 }
