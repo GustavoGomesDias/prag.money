6d7cd0b958fbecbc4fdeda706cc08f1f31c291a1
GustavoGomesDias <treeelevengg@gmail.com>
2022-06-20 18:03:48 -0300

    feat: call handleId

--- src/serverless/decorators/IsValid.ts
+++ src/serverless/decorators/IsValid.ts
@@ -1,3 +1,4 @@
+/* eslint-disable dot-notation */
 /* eslint-disable no-return-await */
 /* eslint-disable func-names */
 /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -8,31 +9,36 @@ import handleId from './helpers/handleId';
 export interface IsValidFields {
   paramName?: string
   fieldIdIsValid?: string | string[]
+  idPosition?: number // for no object fields
   notEmpty?: string[]
   validationValueMsg?: string
   messageError?: string[]
 }
 
 const IsValid = ({
-  notEmpty, fieldIdIsValid, paramName, validationValueMsg, messageError,
+  notEmpty, fieldIdIsValid, paramName, validationValueMsg, messageError, idPosition,
 }: IsValidFields) => (target: any, key: string, descriptor: PropertyDescriptor) => {
   const originalMethod = descriptor.value;
 
   descriptor.value = async function (...args: any) {
     if (fieldIdIsValid) {
-      handleId(fieldIdIsValid, paramName, args);
+      handleId(fieldIdIsValid, idPosition, paramName, args);
+    }
+
+    if (idPosition !== undefined) {
+      handleId(fieldIdIsValid, idPosition, paramName, args);
     }
 
     if (notEmpty && notEmpty.length > 0) {
       let messagePos = 0;
       for (const items of notEmpty) {
+        console.log(args[0][items]);
         const message = messageError ? messageError[messagePos] : 'Todos que tem * é um item obrigatório e deve ser preenchido.';
-        if (paramName) {
-          validationField400code(args[0][items], message);
 
-          if (typeof args[0][items] === 'number') {
-            validationValues(args[0][items] as number, validationValueMsg || 'Valor precisa ser um número e maior/igual que zero.');
-          }
+        validationField400code(args[0][items], message);
+
+        if (typeof args[0][items] === 'number') {
+          validationValues(args[0][items] as number, validationValueMsg || 'Valor precisa ser um número e maior/igual que zero.');
         }
         messagePos++;
       }
