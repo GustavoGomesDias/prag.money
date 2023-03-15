157299255884f92fe416ddb5649467e2a472fb67
Gustavo (Wood) <treeelevengg@gmail.com>
2023-03-14 23:11:50 -0300

    feat: update api url

--- src/services/fetchAPI/init.ts
+++ src/services/fetchAPI/init.ts
@@ -3,7 +3,7 @@ import FetchAPI from './FetchAPI';
 
 const createAPI = (): FetchAPI<HttpResponse> => {
   if (process.env.NODE_ENV === 'production') {
-    return new FetchAPI('https://pragmoney.vercel.app/api');
+    return new FetchAPI('https://pragmoney.woood.dev/api');
   }
 
   return new FetchAPI('http://localhost:3000/api');
